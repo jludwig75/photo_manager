5bc99dee455b9ac1e8ce47e24ec1e5d130608cb2
Jonathan Ludwig <jr.ludwig@gmail.com>
2020-12-16 21:26:41 -0700

    Fixed thumbnail orientation bug. Issue #10

--- imgutils.py
+++ imgutils.py
@@ -1,5 +1,6 @@
 import os
 from PIL import Image
+from PIL.ExifTags import TAGS
 import sys
 
 
@@ -17,16 +18,46 @@ def scaleSize(size):
         height = (size[1] * MAX_WIDTH) / size[0]
     return (int(width), int(height))
 
+ORIENTATION_TAG = 274
+
+def getOrientation(image):
+    exif = image.getexif()
+    if not exif:
+        return None
+    if not ORIENTATION_TAG in exif:
+        return None
+    return exif[ORIENTATION_TAG]
+
 def generateThumbNail(imagePath, destinationFolder):
     image = Image.open(imagePath)
     if not image:
         return False
+    orientation = getOrientation(image)
+    if orientation:
+        print(f'Image orientation: {orientation}')
     newSize = scaleSize(image.size)
     image = image.resize(newSize, Image.LANCZOS)
     imageFileName = imagePath
     if '/' in imageFileName:
         imageFileName = imageFileName[imageFileName.rfind('/')+1:]
     thumbnailPath = os.path.join(destinationFolder, imageFileName)
+    val = [orientation]
+    if 5 in val:
+        val += [4,8]
+    if 7 in val:
+        val += [4, 6]
+    if 3 in val:
+        print("Rotating by 180 degrees.")
+        image = image.transpose(Image.ROTATE_180)
+    if 4 in val:
+        print("Mirroring horizontally.")
+        image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    if 6 in val:
+        print("Rotating by 270 degrees.")
+        image = image.transpose(Image.ROTATE_270)
+    if 8 in val:
+        print("Rotating by 90 degrees.")
+        image = image.transpose(Image.ROTATE_90)
     image.save(thumbnailPath)
 
 
