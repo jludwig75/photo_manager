4d759bf8bf8d6e965ae3b591d660425ebd5cf9df
Jonathan Ludwig <jr.ludwig@gmail.com>
2020-12-22 15:06:19 -0700

    Added folder refresh on timer. Made folder refresh only pick up changes. Issue #11

--- html/components/FolderList.js
+++ html/components/FolderList.js
@@ -24,7 +24,8 @@ app.component('folder-list', {
     data() {
         return {
             folderList: [],
-            selectedFolderName: null
+            selectedFolderName: null,
+            timer: null
         }
     },
     props: {
@@ -34,9 +35,28 @@ app.component('folder-list', {
         }
     },
     methods: {
+        nameInFolderList(folderName) {
+            for (const folder of this.folderList) {
+                if (folderName == folder.name) {
+                    return true;
+                }
+            }
+            return false;
+        },
         updateFolderList(folderList) {
             for (const folderName of folderList) {
-                this.folderList.push({'name': folderName})
+                if (!this.nameInFolderList(folderName)) {
+                    this.folderList.push({'name': folderName});
+                }
+            }
+            var i = this.folderList.length;
+            while(i--) {
+                var folderName = this.folderList[i].name;
+                if (!folderList.includes(folderName)) {
+                    this.folderList.splice(i, 1);
+                }
+            }
+            for (const folderName of this.folderList) {
             }
             if (folderList.length > 0 && this.selectedFolderName == null && this.override_folder_selection == null) {
                 this.onClickFolder(folderList[0]);
@@ -62,14 +82,20 @@ app.component('folder-list', {
             return false;
         },
         refreshFolderList() {
-            this.folderList = [];
+            console.log("Refreshing folder list");
             axios.
                 get('/folders/').
                 then(response => this.updateFolderList(response.data)).
                 catch(error => console.log('Failed to get image list: ' + error));
         }
     },
+    beforeDestroy () {
+        clearInterval(this.timer);
+    },
     mounted() {
         this.refreshFolderList();
+        this.timer = setInterval(() => {
+            this.refreshFolderList();
+        }, 10000);
     }
 })
