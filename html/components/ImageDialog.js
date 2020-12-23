0499f0e9a0c102352bf5362968772b40c59d5d89
Jonathan Ludwig <jr.ludwig@gmail.com>
2020-12-23 12:53:06 -0700

    Set image dialog size based on view port size

--- html/components/ImageDialog.js
+++ html/components/ImageDialog.js
@@ -3,7 +3,7 @@ app.component('image-dialog', {
     /*html*/
 `
 <span v-if="current_image != null">
-    <img v-if="current_image.media_type == 'image'" class="dialog-img" :src="current_image.path">
+    <img v-if="current_image.media_type == 'image'" class="dialog-img" :src="current_image.path" :style="{ maxWidth: imageWidth + 'px', maxHeight: imageHeight + 'px' }">
     <video v-if="current_image.media_type == 'video'" width="853" height="480" controls>
         <source :src="current_image.path" :type="current_image.mime_type">
         Your browser does not support the video tag.
@@ -20,7 +20,13 @@ app.component('image-dialog', {
             type: String,
             required: true
         }
-  },
+    },
+    data() {
+        return {
+            imageWidth: 0,
+            imageHeight: 0
+        }
+    },
     methods: {
         deleteImage() {
             if (confirm('Are you sure you want to delete image"' + this.current_image.name + '"?')) {
@@ -33,6 +39,20 @@ app.component('image-dialog', {
         onDeleteSucceeded() {
             this.$emit('image-deleted', this.current_image);
             $( "#dialog" ).dialog("close");
+        }
+    },
+    mounted() {
+        const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+        const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+        const dialogWidth = Math.floor((vw * 6) / 10);
+        const dialogHeight = vh - 20;
+        this.imageWidth = dialogWidth - 45;
+        if (this.imageWidth < 640) {
+            this.imageWidth = 640;  // Rather have a scrollbar at this size
+        }
+        this.imageHeight = dialogHeight - 90;
+        if (this.imageHeight < 480) {
+            this.imageHeight = 480;  // Rather have a scrollbar at this size
         }
     }
 })
